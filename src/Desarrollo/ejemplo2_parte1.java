7a6baeda69714c7ae491ba2940c7abb6bae15ab1
Gonzalez45 <lopezyessi347@gmail.com>
2022-05-06 12:41:19 -0600

    Terminado mi parte

--- src/Desarrollo/ejemplo2_parte1.java
+++ src/Desarrollo/ejemplo2_parte1.java
@@ -1,24 +1,28 @@
 
 package Desarrollo;
-//Importación de clases para la lectura desde teclado
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class ejemplo1_parte1 {
+
+public class ejemplo2_parte1 {
     public static void main(String[] args) {
         try{ //Objeto leer de la clase BufferedReader
             BufferedReader leer = new BufferedReader (new InputStreamReader(System.in));
             System.out.println("Ingresar la cantidad de su salario:");
             double salario = Double.parseDouble(leer.readLine());
-            ifSencillo(salario);//Envia el valor de salario a aser evaluado al método
+            ifElse(salario); //Envia el valor de salario a aser evaluado al método
         }catch(Exception e){
             System.out.println(e.getMessage());
         }
     }
-    public static void ifSencillo(double salario){
-        if(salario > 235){//La condicio que se evalua y en caso de ser verdadera
+    
+    public static void ifElse(double salario){
+        if(salario > 235){ //La condicio que se evalua y en caso de ser verdadera
             System.out.println("Usted gana mas del salario minimo");
+        }else{ //En caso que la condicion anterior no se cumpla se ejecuta lo siguiente
+            System.out.println("Usted gana menos del salario minimo");
         }
     }
-    
 }
+    
+
